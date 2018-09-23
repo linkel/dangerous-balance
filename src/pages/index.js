3a9cda142459665cf11d678e1400cd24b0921007
klin <ironcarbide@gmail.com>
2018-09-23 11:13:46 -0700

    Index set up to show lists of articles. Added quotations page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,53 +4,18 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
   <div>
-    <h1>On Being Unable to Sacrifice the Puer Aeturnus</h1>
+    <h1>Notes</h1>
     <div>
       <p>
-        From Edward Edinger's “
-        <a href="https://www.goodreads.com/book/show/180671.Ego_and_Archetype">
-          Ego and Archetype
+        <a href= "../zets/pueraeternus">
+          On Being Unable to Sacrifice the Puer Aeturnus
         </a>
-        ”:
-      </p>
-      <blockquote>
-        <p>
-        The psychotherapist frequently sees cases of this sort. Such a person considers himself as a most promising individual. He is full of talents and potentialities. One of his complaints is often that his capacities and interests are too wide-ranging. He is cursed with a plethora of riches. He could do anything but can’t decide on one thing in particular. The problem is that he is all promises and no fulfillment. In order to make a real accomplishment he must sacrifice a number of other potentialities. <br></br> <br></br> He must give up his identification with original unconscious wholeness and voluntarily accept being a real fragment instead of an unreal whole. To be something in reality he must give up being everything in potentia. {" "}
-          <b>
-          The puer aeternus archetype is one of the images of the Self, but to be identified with it means that one never brings any reality to birth.
-          </b>
+        <br></br>
+        <a href= "../zets/quotations">
+          Miscellaneous Quotations
+        </a>
         </p>
-      </blockquote>
-      <p>In a different book by a psychologist, we find another example of the Puer Aeturnus...
-      </p>
-      <p>From "The Defining Decade," by Meg Jay. Chapter 5, "The Customized Life":</p>
-    <blockquote><p>
-    Sessions with Ian did not go smoothly. Like other twentysomethings raised on the promise of doing anything, Ian flinched when faced with the here and now. A life of infinite possibilities had felt like a disorienting and overwhelming burden, but it had also been a liberating fantasy. The concept of “any thing” sounded limitless and exciting, while in comparison digital design sounded limiting… and boring. When we talked about actually moving toward a career in digital design, Ian balked. He did not want to “just have some office job and work nine to five like everyone else.”
-A braver form of self-definition dares to be affirmative. Ian needed to move from talking about what he wasn’t going to do to talking about what he was going to do. “Being against something is easy,” I said. “What are you for?”
-To Ian, claiming was conforming. By starting a career, he imagined he was agreeing to decades of the status quo. Saying yes to one concrete thing felt like saying no to an interesting or limitless life. In fact, it’s the other way around. If Ian didn’t say yes to something, his life was going to become unremarkable and limited.
-I would say, “You need to claim something.”</p><p>
-And Ian would say, “But claiming something feels like losing everything else.”
-Or Ian would say, “I don’t want to settle for some ordinary thing.”
-And I would say, “I’m not talking about settling. I’m talking about starting. Twentysomethings who don’t get started wind up with blank résumés and sounded limiting… and boring. When we talked about actually moving toward a career in digital design, Ian balked. He did not want to “just have some office job and work nine to five like everyone else.”
-Ian was on a sneaky search for glory. He suffered less from the tyranny of the should than he did from the tyranny of the should-not. His life wasn’t about getting A’s or even the need to realize his potential, at least not in any mainstream way. Conventionality wasn’t his niche. Ian’s search for glory was the lure of being different, so he displayed what has been called a common symptom of youth: “the dread of doing what has been done before.” If he ever chose something to do for work, he didn’t want it to be some same-old, everyday thing; his life could be unique.
-I didn’t completely disagree.</p><p>
-Distinctiveness is a fundamental part of identity. We develop a clearer sense of ourselves by firming up the boundaries between ourselves and others. I am who I am because of how I am different from those around me. There is a point to my life because it cannot be carried out in exactly the same way by any other person. Differentness is part of what makes us who we are. It gives our lives meaning.
-But different is simple. Like the easiest way to explain black is to call it the opposite of white, often the first thing we know about ourselves is not what we are—it’s what we aren’t. We mark ourselves as not-this or not-that, the way Ian was quick to say he didn’t want to sit at the same desk all day. But self-definition cannot end there. An identity or a career cannot be built around what you don’t want. We have to shift from a negative identity, or a sense of what I’m not, to a positive one, or a sense of what I am. This takes courage.
-A braver form of self-definition dares to be affirmative. Ian needed to move from talking about what he wasn’t going to do to talking about what he was going to do. “Being against something is easy,” I said. “What are you for?”
-To Ian, claiming was conforming. By starting a career, he imagined he was agreeing to decades of the status quo. Saying yes to one concrete thing felt like saying no to an interesting or limitless life. In fact, it’s the other way around. If Ian didn’t say yes to something, his life was going to become unremarkable and limited.
-When I sat with Ian, I sometimes thought about a thirty-one-year-old woman I worked with who told me she’d spent her twenties changing her hair color as often as she changed jobs: bright purple when answering phones at a spa, white-blond when temping, deep red when Mystery Shopping, chestnut brown when working in a preschool. When she became engaged, she confided that she planned to quit her latest job. She said, “I can’t stand my boss right now. I’ve got a wedding and a honeymoon to plan, and soon I’ll have babies and I’ll get to think about something else for a while.” When I asked how her fiancé, an elementary school teacher, felt about being the sole breadwinner for a soon-to-be growing family, she shrugged nervously.
-And this client made me think of the thirty-nine-year-old female who told me, “At this point in my life, if I’m going to work and pay for child care and be away from my kids all day, I need the work to be interesting and well paid. But I can’t get that sort of job. In my twenties, I didn’t really deal with what I was going to do about work. In my thirties, I had kids. We need the money and I have to work. But you wouldn’t believe the jobs I can’t even get. I go for jobs and people just look at me like, ‘Why haven’t you done something by now?’ I wish someone had told me to think about my résumé a long time ago.”
-Or I thought about the forty-four-year-old male client with a new baby who said to me, “You know, if I’d had a decent psychologist in my twenties, I probably could have started my career before I was thirty-five and had a family before I was in my forties. If you’re still doing this in twenty years, I’m sending my son to you.” When I asked what he would like me to say to his son, my client replied, “That you can’t pull some great career out of a hat in your thirties. You’ve got to start in your twenties.”
-With these clients in mind, over the next many months, my sessions with Ian went more or less this way:
-I would say, “You need to claim something.”</p><p>
-And Ian would say, “But claiming something feels like losing everything else.”
-Or Ian would say, “I don’t want to settle for some ordinary thing.”
-And I would say, “I’m not talking about settling. I’m talking about starting. Twentysomethings who don’t get started wind up with blank résumés and out-of-touch lives only to settle far more down the road. What’s so original about that?”
-
-        </p></blockquote>
-
-    </div>
-    <p>Posted Sep 21, 2018</p>
+      </div>
   </div>
   </Layout>
 )
---
+++ src/pages/zets/quotations.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>Miscellaneous Quotations</h1>
+      <div>
+        <p>Adam Lindsay Gordon:</p>
+        <p>
+          <blockquote>
+            Life is mostly froth and bubble, two things stand like stone.
+            Kindness in another’s trouble, courage in your own.
+          </blockquote>
+        </p>
+
+        <p>Stephen Pinker:</p>
+        <p>
+          <blockquote>
+            Equality is not the empirical claim that all groups of humans are
+            interchangeable; it is the moral principle that individuals should
+            not be judged or constrained by the average properties of their
+            group.
+          </blockquote>
+        </p>
+      </div>
+
+      <p>John Adams, in a letter to Abigail Adams in 1780:</p>
+      <p>
+        {" "}
+        <blockquote>
+          I must study politics and war, that our sons may have liberty to study
+          mathematics and philosophy. Our sons ought to study mathematics and
+          philosophy, geography, natural history and naval architecture,
+          navigation, commerce and agriculture in order to give their children a
+          right to study painting, poetry, music, architecture, statuary,
+          tapestry and porcelain.
+        </blockquote>
+      </p>
+
+      <p>Robert Heinlein, science fiction author:</p>
+
+      <p>
+        <blockquote>
+          Moving parts in rubbing contact require lubrication to avoid excessive
+          wear. Honorifics and formal politeness provide lubrication where
+          people rub together. Often the very young, the untraveled, the naïve,
+          the unsophisticated deplore these formalities as "empty,"
+          "meaningless," or "dishonest," and scorn to use them. No matter how
+          "pure" their motives, they thereby throw sand into machinery that does
+          not work too well at best.
+        </blockquote>
+      </p>
+
+      <p>Dorothy Thompson, American journalist in the 1930s:</p>
+      <p>
+        <blockquote>
+          Courage, it would seem, is nothing less than the power to overcome
+          danger, misfortune, fear, injustice, while continuing to affirm
+          inwardly that life with all its sorrows is good; that everything is
+          meaningful even if in a sense beyond our understanding; and that there
+          is always tomorrow.
+        </blockquote>
+      </p>
+      <p>Posted Sep 23, 2018</p>
+    </div>
+  </Layout>
+);
